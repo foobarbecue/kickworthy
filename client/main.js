f6cf8888d52ea04cf279980c3f1e6384329f4597
Aaron Curtis <foobarbecue@gmail.com>
2014-07-22 18:46:20 -1000

    Add rudimentary search and failed / delivered voting.

--- client/main.js
+++ client/main.js
@@ -1,1 +1,21 @@
- 
+Template.search.events({
+    'keyup input.search-query': function (evt) {
+        Session.set("search-query", evt.currentTarget.value);
+    },
+    'click .delivered' : function(evt, tmpl){
+        Projects.update(this._id,{$inc:{delivered_count:1}})
+    },
+    'click .failed' : function(evt, tmpl){
+        Projects.update(this._id,{$inc:{failed_count:1}})
+    }    
+})
+
+Template.search.helpers({
+    'results' : function(){
+        console.log(Session.get("search-query"));
+        var query = Session.get("search-query");
+        res = Projects.find({$or: [{'name': new RegExp(query)},
+                                    {'blurb': new RegExp(query)}]});
+        return res;
+    }
+})
